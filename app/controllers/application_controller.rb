262489f4d4a14ecabc46e67693ac1cd90d2ce940
Tatsu88-Tokyo <tatsuhiko1028@gmail.com>
2020-05-10 00:16:57 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,10 @@ class ApplicationController < ActionController::Base
       Rails.env.production?
     end
 
+    def basic_auth
+      authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
   def set_ransack
     @q = Product.ransack(params[:q])
