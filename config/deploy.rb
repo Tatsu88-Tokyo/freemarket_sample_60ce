2ef8824702123b1360f4761d2bd04e3182a43276
Tatsu88-Tokyo <tatsuhiko1028@gmail.com>
2020-05-09 23:46:44 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.11.2'
 
 # Capistranoのログの表示に利用する
-set :application, 'freemarket_sample_60c'
+set :application, 'freemarket_sample_60ce'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:Tatsu88-Tokyo/freemarket_sample_60ce.git'
