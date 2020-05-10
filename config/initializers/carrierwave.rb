cb850b1453df134dcbd4bf929ee12d0f1a5396ec
Tatsu88-Tokyo <tatsuhiko1028@gmail.com>
2020-05-10 13:37:21 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,8 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'teamc60'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/teamc60'
+    config.fog_directory  = 'fmarket60c'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fmarket60c'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.secrets.aws_access_key_id,
